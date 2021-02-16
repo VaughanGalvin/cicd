82b2aba5656aea45a3c53d5ccdd6825028a3e4d7
Vaughan Galvin <vaughan.galvin@gmail.com>
2021-02-16 18:28:43 +0300

    coverage added

--- src/test/java/ru/netology/StatisticServiceTest.java
+++ src/test/java/ru/netology/StatisticServiceTest.java
@@ -17,4 +17,15 @@ class StatisticServiceTest {
 
         assertEquals(predicted,actual);
     }
+@Test
+    void findMax2() {
+        StatisticService variable = new StatisticService();
+
+        long[] incomesInEuro = {5, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long predicted = 12;
+
+        long actual = variable.findMax(incomesInEuro);
+
+        assertEquals(predicted,actual);
+    }
 }
